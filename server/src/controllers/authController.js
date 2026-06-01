cfcf927c1a8ee4a8e86aba68e912b01b36d106c9
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-06-01 17:05:15 +0530

    logout controller

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -119,3 +119,24 @@ export const login = async (req,res)=>{
         })
     }
 }
+
+
+export const logout = async (req,res)=>{
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        });
+        return res.status(200).json({
+            message: "Logout successful",
+            success : true,
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: `Error in logout user : ${error.message}`,
+            success : false,
+        })
+    }
+}
