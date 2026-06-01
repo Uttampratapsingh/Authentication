62cca844949bd49df0594a5bb727f9e798a51906
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-06-01 18:10:59 +0530

    middleware for otp controllers to attach user id

---
+++ server/src/middleware/userAuth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+const userAuth = (req,res,next)=>{
+    const token = req.cookies.token;
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized access, token missing",
+            success : false,
+        })    
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({
+            message: "Unauthorized access, invalid token",
+            success : false,
+        })    
+    }
+}
+
+export default userAuth;
