fe54dda9a1edc5f92b4b1b2f5ae321233f303d1b
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-06-03 15:37:16 +0530

    Added navbar to home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import ResetPassword from "./pages/ResetPassword"
+import EmailVerify from "./pages/EmailVerify"
+import Login from "./pages/Login"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
   )
 }
 
